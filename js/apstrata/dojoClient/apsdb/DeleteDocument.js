4cbb00f8b9deb546ee9488fac41c935966d0e35e
rabih.nassar@elementn.com <rabih.nassar@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2009-05-04 11:53:21 +0000

    dojo apsdb client

---
+++ js/apstrata/dojoClient/apsdb/DeleteDocument.js
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ *  Copyright 2009 apstrata
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0.html
+ *  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *  CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations under the License.
+ * *****************************************************************************
+ */
+
+dojo.provide("apstrata.dojoClient.apsdb.DeleteDocument");
+
+dojo.declare("apstrata.dojoClient.apsdb.DeleteDocument",
+[apstrata.dojoClient.apsdb.Get],
+{
+    apsdbOperation: "DeleteDocument",
+
+    execute: function(store, documentKey) {
+	this.request.apsdb.store = store;
+	this.request.apsdb.documentKey = documentKey;
+	
+	this.inherited(arguments);
+    }
+});
+
---
+++ js/apstrata/widgets/WikiEditor.js
@@ -0,0 +1,16 @@
+dojo.provide("apstrata.widgets.WikiEditor");
+
+dojo.declare("apstrata.widgets.WikiEditor",
+	[dijit._Widget, dijit._Templated],
+	{
+		widgetsInTemplate: true,
+//		templatePath: dojo.moduleUrl("apstrata.widgets.wikiEditor", "templates/WikiEditor.html"),
+		templateString: "XXXXXX",
+		breadCrumbs: "",
+		title: "empty page",
+		contents: "",
+		tags: "",
+		
+		// implicit constructor 
+		
+	});
