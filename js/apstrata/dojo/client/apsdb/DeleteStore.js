959f6ba9e9b5fc3909f73a40e7e5ad8470eb1e1a
caroline.farage@elementn.com <caroline.farage@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2009-05-14 13:36:22 +0000

    Adding the copyright notice and a statement of copying permission, saying that the program is distributed under the terms of the GNU Lesser General Public License

--- js/apstrata/dojo/client/apsdb/DeleteStore.js
+++ js/apstrata/dojo/client/apsdb/DeleteStore.js
@@ -1,12 +1,20 @@
 /*******************************************************************************
- *  Copyright 2009 apstrata
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *
- *  You may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0.html
- *  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- *  CONDITIONS OF ANY KIND, either express or implied. See the License for the
- *  specific language governing permissions and limitations under the License.
+ *  Copyright 2009 Apstrata
+ *  
+ *  This file is part of Apstrata Database Javascript Client.
+ *  
+ *  Apstrata Database Javascript Client is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the License,
+ *  or (at your option) any later version.
+ *  
+ *  Apstrata Database Javascript Client is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Apstrata Database Javascript Client.  If not, see <http://www.gnu.org/licenses/>.
  * *****************************************************************************
  */
 
