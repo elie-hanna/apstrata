a5cf136e40018cee26a0b1d3e383baec36acf33e
rabih.nassar@apstrata.com <rabih.nassar@apstrata.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2009-07-07 09:13:15 +0000

    Initial commit of LoginArea

---
+++ apstrataDojoSDK/apstrata/apsdb/client/widgets/LoginArea.js
@@ -0,0 +1,66 @@
+dojo.provide("apstrata.apsdb.client.widgets.LoginArea");
+
+dojo.declare("apstrata.apsdb.client.widgets.LoginArea", 
+	[dijit._Widget, dijit._Templated], 
+	{
+		widgetsInTemplate: true,
+		templateString: "<div><span dojoAttachPoint='spnMessage'></span> <a href='#' dojoAttachPoint='linkLogout'><span dojoAttachPoint='spnLabel'></span></a></div>",
+		connection: null,
+		
+		constructor: function(attrs) {
+			if (attrs.connection) this.connection = attrs.connection
+		},
+		
+		processLoginInfo: function() {
+			var status = this.connection.hasCredentials()
+			var message
+			var label
+			
+			if (status) {
+				label = "logout"
+				
+				if (this.connection.credentials.un!="") {
+					message = "User: " + this.connection.credentials.un
+				} else {
+					message = "Account: " + this.connection.credentials.key
+				}
+			} else {
+				label = "login"
+				message = ""
+			}
+	
+			this.spnMessage.innerHTML = message
+			this.spnLabel.innerHTML = label
+			
+			this.onChange(status)
+		},
+		
+		postCreate: function() {
+			var self = this
+	
+			this.processLoginInfo()
+			
+			dojo.connect(self.linkLogout, "onclick", function() {
+				if (self.connection.hasCredentials()) {
+					self.connection.logout()
+	
+					self.processLoginInfo()
+					//self.domNode.innerHTML = dojo.string.substitute(self.templateString, self)
+				} else {
+	                var login = new apstrata.apsdb.client.widgets.Login(self.connection)		
+	
+	                dojo.connect(login, "loginSuccess", function() {
+						self.processLoginInfo()
+						//self.domNode.innerHTML = dojo.string.substitute(self.templateString, self)
+	                });
+	                dojo.connect(login, "close", function() {
+	                    alert('Adieu mon ami, a bientot!')
+	                });
+	
+				}
+			})
+		},
+		
+		onChange: function(status) {}
+	})
+
