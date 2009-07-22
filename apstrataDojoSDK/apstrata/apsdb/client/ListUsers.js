d709ee454583c407cf4489bf261bb3cfc73222f5
nasr.keyrouz@elementn.com <nasr.keyrouz@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2009-07-22 22:41:48 +0000

    [APSTRATA-524] Adding save,get,list,delete,user.

---
+++ apstrataDojoSDK/apstrata/apsdb/client/ListUsers.js
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ *  Copyright 2009 apstrata
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0.html
+ *  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *  CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations under the License.
+ * *****************************************************************************
+ */
+dojo.provide("apstrata.apsdb.client.ListUsers");
+
+dojo.require("apstrata.apsdb.client.Get");
+
+dojo.declare("apstrata.apsdb.client.ListUsers", [apstrata.apsdb.client.Get], {
+    constructor: function(){
+        this.apsdbOperation = "ListUsers"
+    },
+    
+    execute: function(attrs){
+        if (attrs != undefined && attrs.groupName != undefined) 
+            this.request.apsdb.groupName = attrs.groupName;
+        this.inherited(arguments);
+    }
+});
