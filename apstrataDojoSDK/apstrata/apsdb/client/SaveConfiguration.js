d1f766096f295a9b64e1bc8e24267b9b411288f7
nasr.keyrouz@elementn.com <nasr.keyrouz@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2009-09-02 14:57:25 +0000

    [APSTRATA-618] Adding the js client

---
+++ apstrataDojoSDK/apstrata/apsdb/client/SaveConfiguration.js
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ *  Copyright 2009 apstrata
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0.html
+ *  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *  CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations under the License.
+ * *****************************************************************************
+ */
+
+dojo.provide("apstrata.apsdb.client.SaveConfiguration");
+
+dojo.require("apstrata.apsdb.client.Get");
+
+/**
+ * Allows the creation of account configuration
+ * @class apstrata.apsdb.client.SaveConfiguration
+ * @extends apstrata.apsdb.client.Get
+*/
+dojo.declare("apstrata.apsdb.client.SaveConfiguration",
+[apstrata.apsdb.client.Get],
+{
+    /**
+     * @constructor SaveConfiguration Does not require any parameters
+    */
+    constructor: function() {
+        this.apsdbOperation= "SaveConfiguration"
+    },
+
+    /**
+     * @function execute Saves the createschemaACL attributes in user's profile
+     * @param attrs An array of parameters that must contain the 'createschemaACL' parameter
+     * @throws Error
+     */
+    execute: function(attrs) {
+        if ((attrs != undefined) &&
+            (attrs.createschemaACL != undefined)) {   
+            this.request.createschemaACL = attrs.createschemaACL;
+            this.inherited(arguments);
+        } else throw new Error("apstrata.apsdb.client.SaveConfiguration: missing createschemaACL parameter.")
+    }
+});
+
