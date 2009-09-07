e421fec4286ddf13674484fb9e27748ce85eaa23
rabih.nassar@apstrata.com <rabih.nassar@apstrata.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2009-09-07 01:58:39 +0000

    fixed apstrata.log initialization issue

--- apstrataDojoSDK/apstrata/util/logger/BasicLogger.js
+++ apstrataDojoSDK/apstrata/util/logger/BasicLogger.js
@@ -162,3 +162,11 @@ dojo.declare("apstrata.util.logger.BasicLogger",
 		//console.groupEnd()
 	}
 })
+
+// Init default logger
+if (!window["apstrata"].logger) window["apstrata"].logger = new apstrata.util.logger.BasicLogger()
+// Init shortcut logger
+window["apstrata"].log = function(level, className, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10) {
+	apstrata.logger.log(level, className, [arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10])
+}
+
