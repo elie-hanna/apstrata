467933fb0c662d1ce52756192ac53c7835d1c6de
rabih.nassar@apstrata.com <rabih.nassar@apstrata.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2009-10-14 04:30:29 +0000

    Fixing apstrata.widgets.Alert L&F

--- apstrataDojoSDK/apstrata/widgets/Alert.js
+++ apstrataDojoSDK/apstrata/widgets/Alert.js
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ *  Copyright 2009 Apstrata
+ *  
+ *  This file is part of Apstrata Database Javascript Client.
+ *  
+ *  Apstrata Database Javascript Client is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the License,
+ *  or (at your option) any later version.
+ *  
+ *  Apstrata Database Javascript Client is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Apstrata Database Javascript Client.  If not, see <http://www.gnu.org/licenses/>.
+ * *****************************************************************************
+ */
 dojo.provide("apstrata.widgets.Alert")
 
 dojo.require("dijit._Templated")
@@ -63,7 +82,9 @@ dojo.declare("apstrata.widgets.Alert",
 		var self = this
 
 			if (this._animation) {
-				this.originNode = dojo.byId(this._animation.from)
+				if (dojo.isString(this._animation.from)) this.originNode = dojo.byId(this._animation.from);
+				else this.originNode = this._animation.from
+				
 				this.bounding = dojo.byId(this._animation.bounding)
 			}
 
