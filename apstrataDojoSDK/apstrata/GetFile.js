895c13b6acb2603ac2de50df04861866eda897fa
rabih.nassar@apstrata.com <rabih.nassar@apstrata.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2009-11-09 09:26:03 +0000

    initial checkin of HStackableContainer widgets

---
+++ apstrataDojoSDK/apstrata/GetFile.js
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ *  Copyright 2009 apstrata
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0.html
+ *  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *  CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations under the License.
+ * *****************************************************************************
+ */
+
+dojo.provide("apstrata.GetFile");
+
+dojo.require("apstrata.Operation")
+
+dojo.declare("apstrata.GetFile",
+[apstrata.Operation],
+{
+    /**
+     * @constructor ListStores Does not require any parameters
+    */
+    constructor: function(connection) {
+		this.apsdbOperation = "GetFile"
+    },
+	
+	getUrl: function(attrs) {
+        if (attrs) {
+			this.request = {}
+			this.request.apsdb = {}
+			this.request.apsdb.store = attrs.store	
+			this.request.apsdb.documentKey = attrs.documentKey
+			this.request.apsdb.fieldName = attrs.fieldName
+			this.request.apsdb.fileName = attrs.fileName
+			this.request.apsdb.setContentDisposition = attrs.setContentDisposition
+	
+			return this.buildUrl()
+		} else {
+			throw new Error("apstrata.GetFile: attributes not provided.")
+		}
+	}
+});
