dd67883cd44fbd4135cb803fcc2a2eb09b20c072
caroline.farage@elementn.com <caroline.farage@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2009-11-11 21:36:12 +0000

    JIRA ID [APSTRATA-944] creating the list surveys widget

---
+++ apstrataDojoSDK/demos/survey/widgets/SurveysList.js
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ *  Copyright 2009 Apstrata
+ *  
+ *  This file is part of Apstrata Database Javascript Client.
+ *  
+ *  Apstrata Database Javascript Client is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the License,
+ *  or (at your option) any later version.
+ *  
+ *  Apstrata Database Javascript Client is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Apstrata Database Javascript Client.  If not, see <http://www.gnu.org/licenses/>.
+ * *****************************************************************************
+ */
+
+dojo.provide("surveyWidget.widgets.SurveysList");
+
+dojo.require("dijit._Templated");
+dojo.require("dijit.layout.LayoutContainer");
+	
+dojo.declare("surveyWidget.widgets.SurveyObj",
+	[dijit._Widget, dijit._Templated],
+	{
+		templateString: "<div><a title='' dojoAttachPoint='surveyObject' dojoAttachEvent='onclick: cloneSurvey'>${surveyName}</a></div>",
+		
+		surveyName: "",
+		surveyId: "",
+		
+		constructor: function(attrs) {
+			this.surveyName = attrs.nameParam;
+			this.surveyId = attrs.idParam;
+		},
+		
+		postCreate: function(){
+			this.inherited(arguments);
+		},
+		
+		cloneSurvey: function() {
+			var surveyToClone = new surveyWidget.widgets.Survey();
+			dojo._destroyElement(this.surveysList);
+			this.surveyEdit.addChild(surveyToClone);
+		}
+	});
+
+dojo.declare("surveyWidget.widgets.SurveysList",
+	[dijit._Widget, dijit._Templated],
+	{
+		widgetsInTemplate: true,
+		templatePath: dojo.moduleUrl("surveyWidget.widgets", "templates/SurveysList.html"),
+		username: null,
+		key: null,
+		
+		storeName: "myStore",
+		
+		constructor: function() {
+			if(connection.credentials.username != null && connection.credentials.username != ""){
+				this.username = connection.credentials.username;
+				this.key = connection.credentials.key;
+			}
+		},
+		
+		postCreate: function(){
+			if(this.username != null){
+				//this.title.innerHTML = this.dojoDataModel.title;
+				this.query();
+			}
+			else
+				this.title.innerHTML = "No user is logged in";
+		},
+		
+		query: function() {
+
+			var client = new apstrata.Client({connection: connection});
+			var listing = this;
+
+			var q = client.call({
+				action: "Query",
+				request: {
+					apsdb: {
+						store: listing.storeName,
+						query: "apsdb.creator=\"" + listing.username + "@" + listing.key + "\" and isSurveyMetadata=\"true\"",
+						queryFields: "surveyName,surveySchema,apsdb.documentKey"
+					}
+				},
+				load: function(operation) {
+					listing.display(operation.response);
+				},
+				error: function(operation) {
+					//fail(operation)
+				}
+			});
+			
+		},
+		
+		display: function(data) {
+			
+			var newField = null;
+			var arrSurvey = data.result.documents;
+			
+			for (var doc = 0; doc < arrSurvey.length; doc++) {
+				//newField += "<div><a title=\"Powered by apstrata\" href=# onClick=\"javascript:cloneSurvey('"+arrSurvey[doc].fields[2].values[0]+"')\">"+arrSurvey[doc].fields[0].values[0]+" </a></div>";
+				newField = new surveyWidget.widgets.SurveyObj({
+					nameParam: arrSurvey[doc].fields[0].values[0],
+					idParam: arrSurvey[doc].fields[2].values[0]
+				});
+				this.surveys.addChild(newField);
+			}
+			//this.surveys.innerHTML = newField;
+		}
+	});
---
+++ apstrataDojoSDK/demos/survey/widgets/SurveysListLoader.js
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ *  Copyright 2009 Apstrata
+ *  
+ *  This file is part of Apstrata Database Javascript Client.
+ *  
+ *  Apstrata Database Javascript Client is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the License,
+ *  or (at your option) any later version.
+ *  
+ *  Apstrata Database Javascript Client is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Apstrata Database Javascript Client.  If not, see <http://www.gnu.org/licenses/>.
+ * *****************************************************************************
+ */
+
+var schema = null
+
+function loadjscssfile(filename, filetype){
+	 if (filetype=="js"){ //if filename is a external JavaScript file
+	  var fileref=document.createElement('script')
+	  fileref.setAttribute("type","text/javascript")
+	  fileref.setAttribute("src", filename)
+	 }
+	 else if (filetype=="css"){ //if filename is an external CSS file
+	  var fileref=document.createElement("link")
+	  fileref.setAttribute("rel", "stylesheet")
+	  fileref.setAttribute("type", "text/css")
+	  fileref.setAttribute("href", filename)
+	 }
+	 if (typeof fileref!="undefined")
+	  document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+
+loadjscssfile("http://o.aolcdn.com/dojo/1.3/dojo/resources/dojo.css", "css");
+
+dojo.require("dijit.Declaration");
+dojo.registerModulePath("surveyWidget","../../../../demos/survey");
+dojo.registerModulePath("apstrata","../../../../apstrata");
+dojo.require("surveyWidget.widgets.SurveysList");
+dojo.require("apstrata.Connection")
+dojo.require("apstrata.Client")
+
+var connection = new apstrata.Connection({
+		credentials: {
+			key: apstrata.apConfig.key,
+			username: apstrata.apConfig.username,
+			password: apstrata.apConfig.password
+		},
+		serviceUrl: apstrata.apConfig.serviceURL
+})
