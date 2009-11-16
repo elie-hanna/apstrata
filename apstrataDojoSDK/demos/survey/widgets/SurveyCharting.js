3ff336301ed584684133739186144b0f4e387d62
caroline.farage@elementn.com <caroline.farage@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2009-11-16 16:26:35 +0000

    JIRA ID [APSTRATA-945] Changed the charting of multiple choice questions (Abdallah)

--- apstrataDojoSDK/demos/survey/widgets/SurveyCharting.js
+++ apstrataDojoSDK/demos/survey/widgets/SurveyCharting.js
@@ -115,8 +115,8 @@ dojo.declare("surveyWidget.widgets.SurveyCharting",
 									var choices = fieldDataModel.choices.split(',');
 									fieldValueCounts[i] = new Array(choices.length);
 									for (var j=0; j<choices.length; j++) {
-										fieldValueCounts[i][fieldDataModel.name + '_' + choices[j]] = 0;
-										charting.queryAndDisplayMultipleChoice(fieldDataModel.name, choices[j], fieldResponses[i][j], fieldValueCounts[i]);
+										fieldValueCounts[i][fieldDataModel.name + '_' + choices[j]] = null;
+										charting.queryAndDisplayDelayedTypes(fieldDataModel.name, choices[j], fieldResponses[i][j], fieldValueCounts[i]);
 									}
 									break;
 								case 'radio button':
@@ -125,7 +125,7 @@ dojo.declare("surveyWidget.widgets.SurveyCharting",
 									fieldValueCounts[i] = new Array(choices.length);
 									for (var j=0; j<choices.length; j++) {
 										fieldValueCounts[i][fieldDataModel.name + '_' + choices[j]] = null;
-										charting.queryAndDisplayRadioButton(fieldDataModel.name, choices[j], fieldResponses[i][j], fieldValueCounts[i]);
+										charting.queryAndDisplayDelayedTypes(fieldDataModel.name, choices[j], fieldResponses[i][j], fieldValueCounts[i]);
 									}
 									break;
 								case 'checkbox':
@@ -140,280 +140,12 @@ dojo.declare("surveyWidget.widgets.SurveyCharting",
 									fieldValueCounts[i] = new Array(choices.length);
 									for (var j=0; j<choices.length; j++) {
 										fieldValueCounts[i][fieldDataModel.name + '_' + choices[j]] = null;
-										charting.queryAndDisplayList(fieldDataModel.name, choices[j], fieldResponses[i][j], fieldValueCounts[i]);
+										charting.queryAndDisplayDelayedTypes(fieldDataModel.name, choices[j], fieldResponses[i][j], fieldValueCounts[i]);
 									}
 									break;
 							}
 						}
 					}
-				},
-				error: function(operation) {
-					//fail(operation)
-				}
-			});
-		},
-
-		/**
-		 * Queries apstrata database for the count of fields that have the passed field name and value.
-		 * Call a query to count the survey takers using the ([fieldName] = [each of the passed choices]) value for the query expression.
-		 * The (surveysTakenCount - [this count]) is the number of people who did not check this radio button.
-		 *
-		 * @param fieldName The name of the field to use in the query expression
-		 * @param fieldValue The value of the field to use in the query expression
-		 * @param callbackResult The callback result container, it is only used to differentiate the callback of every XHR when it returns
-		 * @param fieldValueCounts The array location to store the field value counts
-		 */
-		queryAndDisplayRadioButton: function (fieldName, fieldValue, callbackResult, fieldValueCounts) {
-			var client = new apstrata.Client({connection: connection});
-			var charting = this;
-
-			var queryRequest = {
-				apsdb: {
-					store: charting.storeName,
-					query: "apsdb.objectName=\"" + charting.apsdbSchema + "\" AND " + fieldName + "=\"" + fieldValue + "\"",
-					queryFields: "*",
-					count: true
-				}
-			};
-
-			callbackResult = client.call({
-				action: "Query",
-				request: queryRequest,
-				load: function(operation) {
-					var valueCount = operation.response.result.count;
-					fieldValueCounts[fieldName + '_' + fieldValue] = valueCount; // Save the returned value count
-
-					// Loop over the existing field value counts and if all values have been accounted for, then display the chart
-					var areAllValuesPresent = true;
-					for (fieldValue in fieldValueCounts) {
-						if (fieldValueCounts[fieldValue] == null) {
-							areAllValuesPresent = false;
-							break;
-						}
-					}
-
-					// If all radio button values have returned their count, then display this field's chart
-					if (areAllValuesPresent) {
-						var chartLine = charting.displayTable.lastChild; // Get the last DIV in the display table
-						var chartCell = null;
-						// Create a new line if: (1) We couldn't find one, (2) We found a default text node, (3) The caller is asking for a new line
-						if (chartLine == null || chartLine.nodeType == 3 || charting.isNewLine) {
-							chartLine = document.createElement('DIV');
-							chartCell = document.createElement('DIV');
-							chartLine.className = 'surveyChartLineSize';
-							chartLine.appendChild(chartCell);
-							charting.floatElement(chartCell, 'left');
-
-							charting.displayTable.appendChild(chartLine); // Add the new line with the new cell to the display table
-						} else {
-							var chartCell = document.createElement('DIV');
-							charting.floatElement(chartCell, 'left');
-
-							chartLine.appendChild(chartCell); // Just add the new cell to the existing line
-						}
-
-						// 1- Add the chart title to the chart cell
-						var chartTitle = document.createElement('DIV');
-						chartTitle.style.width = '220px';
-						chartTitle.style.paddingLeft = '10px';
-						var fieldTitle = charting.getTitleOf(fieldName);
-						chartTitle.innerHTML = fieldTitle;
-						chartCell.appendChild(chartTitle);
-
-						// 2- Add the chart placeholder in the chart cell
-						var chartDIV = document.createElement('DIV');
-						chartDIV.setAttribute('id', fieldName);
-						chartDIV.className = 'surveyChartSize';
-						chartCell.appendChild(chartDIV);
-
-						// 3- Create and render the chart
-						var chart = new dojox.charting.Chart2D(fieldName);
-/*						//TODO: Replace this code with the code below it in order to get pie charts instead of horizontal bar graphs
-						chart.setTheme(dojox.charting.themes.PlotKit.red);
-						chart.addPlot('default', {
-							type: 'Pie',
-							font: 'normal normal bold 8pt Tahoma',
-							fontColor: 'white',
-							labelOffset: 40
-						});
-
-						var valuesArr = new Array(fieldValueCounts.length);
-						var k = 0;
-						for (fieldValue in fieldValueCounts) {
-							var chartlabel = fieldValue.substring(fieldValue.lastIndexOf('_') + 1, fieldValue.length);
-							valuesArr[k] = {y: fieldValueCounts[fieldValue], text: fieldValueCounts[fieldValue] + ' ' + chartlabel};
-							k++;
-						}
-						chart.addSeries('Series A', valuesArr);*/
-						chart.addAxis("x", {
-							fixLower: 'major',
-							fixUpper: 'major',
-							minorTick: {stroke: "black", length: 0},
-							minorLabels: false,
-							includeZero: true
-						});
-						// Construct the labels and values arrays
-						var lablesArr = new Array(fieldValueCounts.length + 1);
-						var valuesArr = new Array(fieldValueCounts.length);
-						var k = 0;
-						lablesArr[k] = {value: 0, text: ''};
-						for (fieldValue in fieldValueCounts) {
-							var chartlabel = fieldValue.substring(fieldValue.lastIndexOf('_') + 1, fieldValue.length);
-							lablesArr[k + 1] = {value: k + 1, text: chartlabel};
-							valuesArr[k] = fieldValueCounts[fieldValue] * 1;
-							k++;
-						}
-						chart.addAxis("y", {
-							vertical: true, 
-							fixLower: "none", 
-							fixUpper: "none", 
-							natural: true,
-							majorTick: { length: 3 },
-							labels: lablesArr
-						});
-
-						// Set the gap between the bars according to the number of bars, i.e. More bars = Smaller gap
-						var gapBetweenBars = 0;
-						if (fieldValueCounts.length < 4) gapBetweenBars = 11;
-						else if (fieldValueCounts.length < 7) gapBetweenBars = 9;
-						else if (fieldValueCounts.length > 6) gapBetweenBars = 3;
-						chart.addPlot("default", { type: "Bars", gap: gapBetweenBars });
-
-						var inverseCount = charting.surveysTakenCount - valueCount;
-						chart.addSeries(
-							"Series A", 
-							valuesArr,
-							{ stroke: {width: 0}, fill: '#ff6600' }
-						);
-						var animA = new dojox.charting.action2d.Highlight(chart, "default", {
-							duration: 450,
-							easing:   dojo.fx.easing.bounceOut
-						});
-						var animB = new dojox.charting.action2d.Tooltip(chart, "default");
-						chart.render();
-
-						// Alternate the isNewLine variable to show the charts in two columns
-						charting.isNewLine = (charting.isNewLine) ? false : true;
-					}
-				},
-				error: function(operation) {
-					//fail(operation)
-				}
-			});
-		},
-
-		/**
-		 * Queries apstrata database for the count of fields that have the passed field name and value.
-		 * Call a query to count the survey takers using the ([fieldName] = [each of the passed choices]) value for the query expression.
-		 * The (surveysTakenCount - [this count]) is the number of people who did not check this box.
-		 *
-		 * @param fieldName The name of the field to use in the query expression
-		 * @param fieldValue The value of the field to use in the query expression
-		 * @param callbackResult The callback result container, it is only used to differentiate the callback of every XHR when it returns
-		 * @param fieldValueCounts The array location to store the field value counts
-		 */
-		queryAndDisplayMultipleChoice: function (fieldName, fieldValue, callbackResult, fieldValueCounts) {
-			var client = new apstrata.Client({connection: connection});
-			var charting = this;
-
-			var queryRequest = {
-				apsdb: {
-					store: charting.storeName,
-					query: "apsdb.objectName=\"" + charting.apsdbSchema + "\" AND " + fieldName + "=\"" + this.clean(fieldValue) + "\"",
-					queryFields: "*",
-					count: true
-				}
-			};
-
-			callbackResult = client.call({
-				action: "Query",
-				request: queryRequest,
-				load: function(operation) {
-					var valueCount = operation.response.result.count;
-					fieldValueCounts[fieldName + '_' + fieldValue] = valueCount; // Save the returned value count
-
-					var chartLine = charting.displayTable.lastChild; // Get the last DIV in the display table
-					var chartCell = null;
-					// Create a new line if: (1) We couldn't find one, (2) We found a default text node, (3) The caller is asking for a new line
-					if (chartLine == null || chartLine.nodeType == 3 || charting.isNewLine) {
-						chartLine = document.createElement('DIV');
-						chartCell = document.createElement('DIV');
-						chartLine.className = 'surveyChartLineSize';
-						chartLine.appendChild(chartCell);
-						charting.floatElement(chartCell, 'left');
-
-						charting.displayTable.appendChild(chartLine); // Add the new line with the new cell to the display table
-					} else {
-						var chartCell = document.createElement('DIV');
-						charting.floatElement(chartCell, 'left');
-
-						chartLine.appendChild(chartCell); // Just add the new cell to the existing line
-					}
-
-					// 1- Add the chart title to the chart cell
-					var chartTitle = document.createElement('DIV');
-					chartTitle.style.width = '220px';
-					chartTitle.style.paddingLeft = '10px';
-					var fieldTitle = charting.getTitleOf(fieldName);
-					chartTitle.innerHTML = fieldTitle + ' : ' + fieldValue;
-					chartCell.appendChild(chartTitle);
-
-					// 2- Add the chart placeholder in the chart cell
-					var chartDIV = document.createElement('DIV');
-					chartDIV.setAttribute('id', fieldName + '_' + fieldValue);
-					chartDIV.className = 'surveyChartSize';
-					chartCell.appendChild(chartDIV);
-
-					// 3- Create and render the chart
-					var chart = new dojox.charting.Chart2D(fieldName + '_' + fieldValue);
-/*					//TODO: Replace this code with the code below it in order to get pie charts instead of horizontal bar graphs
-					chart.setTheme(dojox.charting.themes.PlotKit.red);
-					chart.addPlot('default', {
-						type: 'Pie',
-						font: 'normal normal bold 8pt Tahoma',
-						fontColor: 'white',
-						labelOffset: 40
-					});
-					var inverseCount = charting.surveysTakenCount - valueCount;
-					chart.addSeries('Series A', [
-						{y: valueCount, text: valueCount + ' Yes', color: 'blue'},
-						{y: inverseCount, text: inverseCount + ' No', color: 'red'}
-					]);*/
-					chart.addAxis("x", {
-						fixLower: 'major',
-						fixUpper: 'major',
-						minorTick: {stroke: "black", length: 0},
-						minorLabels: false,
-						includeZero: true
-					});
-					chart.addAxis("y", {
-						vertical: true, 
-						fixLower: "none", 
-						fixUpper: "none", 
-						natural: true,
-						majorTick: { length: 3 },
-						labels: [
-							{value: 0, text: ''},
-							{value: 1, text: 'Yes'}, 
-							{value: 2, text: 'No'}
-						]
-					});
-					chart.addPlot("default", { type: "Bars", gap: 16 });
-					var inverseCount = charting.surveysTakenCount - valueCount;
-					chart.addSeries(
-						"Series A", 
-						[ valueCount * 1, inverseCount ],
-						{ stroke: {width: 0}, fill: '#ff6600' }
-					);
-					var animA = new dojox.charting.action2d.Highlight(chart, "default", {
-						duration: 450,
-						easing:   dojo.fx.easing.bounceOut
-					});
-					var animB = new dojox.charting.action2d.Tooltip(chart, "default");
-					chart.render();
-
-					// Alternate the isNewLine variable to show the charts in two columns
-					charting.isNewLine = (charting.isNewLine) ? false : true;
 				},
 				error: function(operation) {
 					//fail(operation)
@@ -543,21 +275,24 @@ dojo.declare("surveyWidget.widgets.SurveyCharting",
 		/**
 		 * Queries apstrata database for the count of fields that have the passed field name and value.
 		 * Call a query to count the survey takers using the ([fieldName] = [each of the passed choices]) value for the query expression.
-		 * The (surveysTakenCount - [this count]) is the number of people who did not choose this list item.
+		 * For:
+		 * 1- List: The (surveysTakenCount - [this count]) is the number of people who did not choose this list item.
+		 * 2- Multiple Choice: The (surveysTakenCount - [this count]) is the number of people who did not check this box.
+		 * 3- Radio Buttons: The (surveysTakenCount - [this count]) is the number of people who did not check this radio button.
 		 *
 		 * @param fieldName The name of the field to use in the query expression
 		 * @param fieldValue The value of the field to use in the query expression
 		 * @param callbackResult The callback result container, it is only used to differentiate the callback of every XHR when it returns
 		 * @param fieldValueCounts The array location to store the field value counts
 		 */
-		queryAndDisplayList: function (fieldName, fieldValue, callbackResult, fieldValueCounts) {
+		queryAndDisplayDelayedTypes: function (fieldName, fieldValue, callbackResult, fieldValueCounts) {
 			var client = new apstrata.Client({connection: connection});
 			var charting = this;
 
 			var queryRequest = {
 				apsdb: {
 					store: charting.storeName,
-					query: "apsdb.objectName=\"" + charting.apsdbSchema + "\" AND " + fieldName + "=\"" + fieldValue + "\"",
+					query: "apsdb.objectName=\"" + charting.apsdbSchema + "\" AND " + fieldName + "=\"" + this.clean(fieldValue) + "\"",
 					queryFields: "*",
 					count: true
 				}
