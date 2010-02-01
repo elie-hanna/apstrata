2d5b57286e220b40c2cc92faacc3ae40e451a09d
rabih.nassar@apstrata.com <rabih.nassar@apstrata.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2010-02-01 16:16:06 +0000

    initial checkin genericEmbeddable

---
+++ apstrataDojoSDK/demos/genericEmbeddable/apstrata/genericEmbeddable/GenericEmbeddable.js
@@ -0,0 +1,8 @@
+/**
+ * @author rabih
+ */
+dojo.declare("apstrata.genericEmbeddable.GenericEmbeddable")
+
+dojo.define("apstrata.genericEmbeddable.GenericEmbeddable", [dijit._Widget, dijit._Templated], {
+	templateString: "<div><b>I'm embedded</b></div>"
+})
