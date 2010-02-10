7b8ff71da32f787cb77be78ca60fedeeddd94b25
abdallah.chamas@elementn.com <abdallah.chamas@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2010-02-10 00:26:37 +0000

    JIRA ID [APSTRATA-1136] Initial commit of the mForms registration

--- apstrataDojoSDK/demos/mForms/apstrata/mForms/MForms.js
+++ apstrataDojoSDK/demos/mForms/apstrata/mForms/MForms.js
@@ -21,6 +21,7 @@ dojo.provide("apstrata.mForms.MForms")
 dojo.provide("apstrata.mForms.MainPanel")
 
 dojo.require('dojox.widget.Toaster')
+dojo.require("dojo.parser");
 
 dojo.require("apstrata.Client")
 dojo.require("apstrata.horizon.Login")
@@ -28,7 +29,6 @@ dojo.require("apstrata.horizon.Login")
 dojo.require("apstrata.StickyConnection")
 
 dojo.require("apstrata.horizon.HStackableMainPanel")
-dojo.require("apstrata.horizon.Login")
 dojo.require("apstrata.horizon.Preferences")
 
 dojo.require("apstrata.mForms.Campaigns")
@@ -197,8 +197,166 @@ dojo.declare("apstrata.mForms.Login",
 	
 	loginUser: function() {
 		this.inherited(arguments)
-	}
+	},
 	
+	/**
+	 * Closes the existing panel and opens the registration panel.
+	 * NOTE: Called from the "Register here" link in the Login.html file
+	 */
+	switchToRegisterPanel: function () {
+		this.close();
+		this.openPanel(apstrata.mForms.RegistrationPanel);
+	}
+})
+
+dojo.declare("apstrata.mForms.RegistrationPanel", 
+[dijit._Widget, dojox.dtl._Templated, apstrata.horizon._HStackableMixin], 
+{
+	widgetsInTemplate: true,
+	templatePath: dojo.moduleUrl("apstrata.mForms", "templates/Registration.html"),
+
+	maximizePanel: true,
+	
+	constructor: function() {
+		var config = new apstrata.mForms.ApConfig()
+		
+		this.key = {}
+		this.key.value = config.get().key
+	},
+	
+	_onMouseoverUser: function() {
+		dojo.style(this.dvRegisterUser, {background: "#AAAADD"})
+	},
+	
+	_onMouseoutUser: function() {
+		dojo.style(this.dvRegisterUser, {background: "#DDDDee"})
+	},
+	
+	postCreate: function() {
+		console.debug(this.templatePath)
+		this.inherited(arguments)
+	},
+	
+	/**
+	 * 1- Call script API to generate a registration code and call the SaveDocument API under the current authenticating
+	 *    owner with the registration code as a field, then send an email to the user with the registration code and return
+	 *    the document key in the result
+	 * 2- If calling the script API is successful, then remove the registration panel and put the verify registration panel
+	 * 3- If calling the script API is NOT successful, then show an error message with the code and detail
+	 */
+	registerUser: function() {
+		if (this.registerUserForm.validate()) {
+			var self = this;
+			if (self.password.value != self.reenteredPassword.value) {
+				self.errorMsg.innerHTML = 'Passwords do not match';
+				return;
+			}
+
+			var client = new apstrata.Client({connection: connection});
+
+			var scriptRequest = dojo.mixin(
+				{
+					apsdb: {
+						scriptName: "verifyMFormsUser1",
+					}
+				},
+				{
+					firstName: self.firstName.value,
+					lastName: self.lastName.value,
+					password: self.password.value,
+					username: self.username.value,
+					email: self.email.value
+				});
+
+			// Call the verifyMFormsUser1 script to save a document with the registration code and send an email to the user
+			var verifyUserScript1 = client.call({
+				action: "RunScript",
+				request: scriptRequest,
+				load: function(operation) {
+					if (operation.result.status == 'success') {
+						var documentKey = operation.result.documentKey;
+						self.close();
+						self.openPanel(apstrata.mForms.VerifyRegistrationPanel, documentKey);
+					} else {
+						//operation.result.errorDetail;
+					}
+				},
+				error: function(operation) {
+					//fail(operation)
+				}
+			});
+		}
+	}
+})
+
+dojo.declare("apstrata.mForms.VerifyRegistrationPanel", 
+[dijit._Widget, dojox.dtl._Templated, apstrata.horizon._HStackableMixin], 
+{
+	widgetsInTemplate: true,
+	templatePath: dojo.moduleUrl("apstrata.mForms", "templates/VerifyRegistration.html"),
+
+	maximizePanel: true,
+	documentKey: "",
+	
+	constructor: function(documentKeyParam) {
+		var config = new apstrata.mForms.ApConfig()
+		
+		this.key = {}
+		this.key.value = config.get().key
+		this.documentKey = documentKeyParam;
+	},
+	
+	_onMouseoverUser: function() {
+		dojo.style(this.dvRegisterUser, {background: "#AAAADD"})
+	},
+	
+	_onMouseoutUser: function() {
+		dojo.style(this.dvRegisterUser, {background: "#DDDDee"})
+	},
+	
+	postCreate: function() {
+		console.debug(this.templatePath)
+		this.inherited(arguments)
+	},
+	
+	/**
+	 * 1- When the user clicks on the verify button in the VerifyRegistrationPanel, we call another script with the
+	 *    registration code, and document key that the user input and validate that they are the same in the saved document
+	 *    through the script
+	 * 2- If they are the same, then continue to wherever the user wanted to go before he registered
+	 * 3- If they are the NOT same, then display an error message saying that the registration code is invalid
+	 */
+	verifyUser: function() {
+		if (this.registerUserForm.validate()) {
+			var client = new apstrata.Client({connection: connection});
+			var self = this;
+
+			var scriptRequest = dojo.mixin(
+				{
+					apsdb: {
+						scriptName: "verifyMFormsUser2",
+					}
+				},
+				{
+					registrationCode: self.registrationCode.value,
+					documentKey: self.documentKey
+				});
+
+			// Call the verifyMFormsUser2 script to verify that the registration code is correct, then create the user
+			var verifyUserScript2 = client.call({
+				action: "RunScript",
+				request: scriptRequest,
+				load: function(operation) {
+					if (operation.result.status != 'success') {
+						self.errorMsg.innerHTML = operation.result.errorDetail;
+					}
+				},
+				error: function(operation) {
+					//fail(operation)
+				}
+			});
+		}
+	}
 })
 
 
