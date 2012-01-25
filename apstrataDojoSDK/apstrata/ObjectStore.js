a1c1e9854e8b1f608aa922a067eb6c9fb57b3ad5
rabih.nassar@elementn.com <rabih.nassar@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2012-01-25 10:30:42 +0000

    added the remove method to ObjectStore

--- apstrataDojoSDK/apstrata/ObjectStore.js
+++ apstrataDojoSDK/apstrata/ObjectStore.js
@@ -1,4 +1,4 @@
-/*******************************************************************************
+ /*******************************************************************************
  *  Copyright 2011 Apstrata
  *  
  *  This file is part of Apstrata Database Javascript Client.
@@ -17,6 +17,12 @@
  *  along with Apstrata Database Javascript Client.  If not, see <http://www.gnu.org/licenses/>.
  * *****************************************************************************
  */
+
+/**
+ * apstrata.ObjectStore provides a dojo Store implementation capable of interacting with the apstrata service
+ * 
+ * @fileOverview
+ */
 dojo.provide("apstrata.ObjectStore")
 
 dojo.require("apstrata.Connection")
@@ -28,8 +34,15 @@ dojo.require("dojo.data.ObjectStore")
 dojo.declare("apstrata.ObjectStore", 
 [dojo.store.api.Store], 
 {
+	
 	idProperty: "apsdb.documentKey",
 	
+	/**
+	 * Instantiates a new instance of ObjectStore
+	 * <br>
+	 * 
+	 * @param {Object} options has
+	 */
 	constructor: function(options) {
 		var self = this
 		
@@ -45,19 +58,39 @@ dojo.declare("apstrata.ObjectStore",
 	query: function(attrs, options) {
 		var deferred = new dojo.Deferred();
 
-		var apsdb = attrs || {};
-		apsdb.store = this.store;
-		apsdb.queryFields = this.queryFields;
-		apsdb.query = this.queryFilter;
-		if (this.resultsPerPage) apsdb.resultsPerPage = this.resultsPerPage;
-
+		var apsdb = attrs || {}
+		apsdb.store = this.store
+		if (this.ftsQuery) apsdb.ftsQuery = this.ftsQuery
+		if (this.queryExpression) apsdb.query = this.queryExpression
+		if (this.queryFields) apsdb.queryFields = this.queryFields
+		if (this.resultsPerPage) apsdb.resultsPerPage = this.resultsPerPage
+		if (options.count) apsdb.resultsPerPage = options.count
+		
+		if (options.sort) {
+			var type = 'string'
+			
+			if (this.fieldTypes && this.fieldTypes[options.sort[0].attribute]) type = this.fieldTypes[options.sort[0].attribute]
+			
+			apsdb.sort = options.sort[0].attribute + " <" + type +":" + (options.sort[0].descending?"DESC":"ASC") + ">"
+		}
+		
+		
+		if (options.start) {
+			apsdb.pageNumber = options.start/options.count + 1 	
+		} else {
+			apsdb.pageNumber = 1
+		}
+		
+		// TODO: this needs to be managed smarter so we don't ask the count on each query
+		apsdb.count = true//(apsdb.pageNumber == 1)
+	
 		var queryAttrs = {
 			action: "Query",
-			useHttpMethod: "POST",
 			request: {
 				apsdb: apsdb
 			},
 			load: function(operation) {
+				operation.response.result.documents.total = operation.response.result.count
 				deferred.callback(operation.response.result.documents)
 			},
 			error: function(operation) {
@@ -67,7 +100,7 @@ dojo.declare("apstrata.ObjectStore",
 	
 		this.client.call(queryAttrs)
 		
-		return dojo.store.util.QueryResults(deferred)
+		return this.queryResults(deferred)
 	},
 	
 	getIdentity: function(object) {
@@ -84,7 +117,6 @@ dojo.declare("apstrata.ObjectStore",
 		
 		var queryAttrs = {
 			action: "Query",
-			useHttpMethod: "POST",
 			request: {
 				apsdb: apsdb
 			},
@@ -98,7 +130,7 @@ dojo.declare("apstrata.ObjectStore",
 	
 		this.client.call(queryAttrs)
 		
-		return dojo.store.util.QueryResults(deferred)
+		return this.queryResults(deferred)
 	},
 	
 	put: function(object, options) {
@@ -111,8 +143,82 @@ dojo.declare("apstrata.ObjectStore",
 	},
 
 	remove: function(id) {
-		console.dir(object)
-	}			
+		var self = this
+		var deferred = new dojo.Deferred();
+		
+		var attrs = {
+			action: "DeleteDocument",
+			request: {
+				apsdb: {
+					store: self.store,
+					documentKey: id
+				}
+			},
+			load: function(operation) {
+				deferred.callback(operation.response.result)
+			},
+			error: function(operation) {
+				deferred.callback(null)
+			}
+		}
+	
+		this.client.call(attrs)
+		
+		return deferred
+	},
+	
+	queryResults: function(results){
+		// summary:
+		//		A function that wraps the results of a store query with additional
+		//		methods.
+		//
+		// description:
+		//		QueryResults is a basic wrapper that allows for array-like iteration
+		//		over any kind of returned data from a query.  While the simplest store
+		//		will return a plain array of data, other stores may return deferreds or
+		//		promises; this wrapper makes sure that *all* results can be treated
+		//		the same.
+		//
+		//		Additional methods include `forEach`, `filter` and `map`.
+		//
+		// returns: Object
+		//		An array-like object that can be used for iterating over.
+		//
+		// example:
+		//		Query a store and iterate over the results.
+		//
+		//	|	store.query({ prime: true }).forEach(function(item){
+		//	|		//	do something
+		//	|	});
+		
+		if(!results){
+			return results;
+		}
+		// if it is a promise it may be frozen
+		if(results.then){
+			results = dojo.delegate(results);
+		}
+		function addIterativeMethod(method){
+			if(!results[method]){
+				results[method] = function(){
+					var args = arguments;
+					return dojo.when(results, function(results){
+						Array.prototype.unshift.call(args, results);
+						return dojo.store.util.QueryResults(dojo[method].apply(dojo, args));
+					});
+				};
+			}
+		}
+		addIterativeMethod("forEach");
+		addIterativeMethod("filter");
+		addIterativeMethod("map");
+		if(!results.total){
+			results.total = dojo.when(results, function(results){
+				return results.total;
+			});
+		}
+		return results;
+	}		
 })
 
 dojo.declare("apstrata.ObjectStoreAdaptor", 
@@ -124,4 +230,6 @@ dojo.declare("apstrata.ObjectStoreAdaptor",
 		this.objectStore.put(object)
 	}
 })
+
+
 
