ea2ee0bdece6d7951b22a9632f2e59af1413a5f8
abdallah.chamas@elementn.com <abdallah.chamas@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2012-02-07 13:20:08 +0000

    JIRA ID [PTBURST-549] Added a title to the popup.

--- apstrataDojoSDK/apstrata/widgets/Alert.js
+++ apstrataDojoSDK/apstrata/widgets/Alert.js
@@ -27,7 +27,7 @@ dojo.require('dojo.fx.easing');
 dojo.declare("apstrata.widgets.Alert", 
 [dijit._Widget, dijit._Templated], 
 {
-	templateString:"<div class='Alert'><div class='alertContent'><div class='AlertIcon' dojoAttachPoint='icon'></div><div class='AlertContent' dojoAttachPoint='content' dojoAttachEvent='onmouseover: _showButtons, onmouseout: _hideButtons'>Content</div><div class='AlertControl' dojoAttachEvent='onmouseover: _showButtons' dojoAttachPoint='control'></div></div></div>",
+	templateString:"<div class='Alert'><div class='alertContent'><h1 class='alertTitle' dojoAttachPoint='title'></h1><div class='AlertIcon' dojoAttachPoint='icon'></div><div class='AlertContent' dojoAttachPoint='content' dojoAttachEvent='onmouseover: _showButtons, onmouseout: _hideButtons'>Content</div><div class='AlertControl' dojoAttachEvent='onmouseover: _showButtons' dojoAttachPoint='control'></div></div></div>",
 	width: 200,
 	height: 200,
 	animation: null,
@@ -43,6 +43,7 @@ dojo.declare("apstrata.widgets.Alert",
 		if (attrs) {
 			if (attrs.width) this.width = attrs.width
 			if (attrs.height) this.height = attrs.height
+			if (attrs.title) this.titleStr = attrs.title;
 			if (attrs.message) this.message = attrs.message
 			if (attrs.iconSrc) this.iconSrc = attrs.iconSrc
 			
@@ -202,6 +203,9 @@ dojo.declare("apstrata.widgets.Alert",
 			});
 		}
 
+		if (self.titleStr) {
+			self.title.innerHTML = self.titleStr;
+		}
 		self.content.innerHTML = self.message
 
 		this._animation = {
