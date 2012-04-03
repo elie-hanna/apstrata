06b0457f036fe88b6fb26cc9651650bc46db3c4c
rabih.nassar@elementn.com <rabih.nassar@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2012-04-03 20:12:50 +0000

    adding apstrata.widgets.forms.FormGenerator artifacts

--- apstrataDojoSDK/apstrata/widgets/forms/FieldSet.js
+++ apstrataDojoSDK/apstrata/widgets/forms/FieldSet.js
@@ -5,6 +5,9 @@ dojo.require("dijit.form.SimpleTextarea")
 dojo.require("dijit.form.ComboBox")
 dojo.require("dijit.form.Button")
 
+dojo.require("dojo.store.Memory")
+dojo.require("dojo.data.ObjectStore")
+
 dojo.declare("apstrata.widgets.forms.FieldSet", 
 [dijit._Widget, dojox.dtl._Templated], 
 {
@@ -13,28 +16,159 @@ dojo.declare("apstrata.widgets.forms.FieldSet",
 	
 	_fieldSet: [],
 	label: "",
+
+	//
+	// Constants
+	//
+	_FORM: "form",
+	_SUBFORM: "subform",
+	_ROW: "row",
 	
+	//
+	// Private vars
+	//
+	_rows: 0,
+
+	/**
+	 * Instantiates a Collection of single or multi value fields
+	 * 
+	 * @param {Object} options.fieldset
+	 * @param {Object} options.cardinality
+	 * @param {Object} options.type
+	 * @param {Object} options.style
+	 * @param {Object} options.cardinalityValues
+	 * 
+	 */
 	constructor: function(options) {
 		this.fieldset = options.fieldset
-		this._type = options.type
+		this.formGenerator = options.formGenerator
+		
+		if (options.cardinality) {
+			this.cardinality = options.cardinality
+			
+			if (!options.cardinality.min) this.cardinality.min = 0 // minimum cardinality is 1
+			if (options.cardinality.max < options.cardinality.min) this.cardinality.max = this.cardinality.min 
+		} else this.cardinality = {min:1, max:1}
+		
+		this.type = options.type
+		if (options.style) this.style = options.style; else this.style = this._FORM
+		
+		if (options.cardinalityValues) {
+			this.cardinalityValues = options.cardinalityValues
+		}
+		
+		for (key in this.formGenerator.cardinalityValues) {
+			for (var i=0; i<options.fieldset.length; i++) {
+				if (options.fieldset[i].name == key) {
+					this.cardinality.value = this.formGenerator.cardinalityValues[key]
+					break
+				}
+			}
+		}
 	},
 	
-	addSet: function() {
+	postCreate: function(options) {
 		var self = this
 		
-		var dv = dojo.create("div")
-		dojo.addClass(dv, "set")
-		dojo.place(dv, this.dvFields)
+		// If this fieldset is to be represented as a row, let's add a header for labels
+		if (this.style == this._ROW) this._addHeaderRow()
+		
+		// Calculate startRows number of rows depending on min or the amount of values of each multi value field
+		var startRows
+		if (this.cardinality.value) {
+			if (this.cardinality.min<this.cardinality.value) {
+				startRows = this.cardinality.value
+			} else {
+				startRows = this.cardinality.min
+			}
+		} else startRows = this.cardinality.min
+
+		// Add the rows
+		for (var i=0; i<startRows; i++) this._addSet(i)
+
+		// Add the bottom disabled row used as a place holder
+		if (this.style == this._ROW) this._addSet(0, true)
+	},
+
+	//
+	// Private methods
+	//
+	
+	/**
+	 * 
+	 * @param {boolean} hide   disables the add button
+	 */
+	_hideAddButton: function(hide) {
+		if (hide) this._addButton.set("disabled", "disabled"); else this._addButton.set("disabled", "")
+	},
+	
+	/**
+	 * Add the label row for multivalue fields
+	 */
+	_addHeaderRow: function() {
+		// only if there are more than 1 columns
+		if (this.fieldset.length>1) {
+			var dv = dojo.create("div")
+			dojo.addClass(dv, "headerRow")
+			dojo.place(dv, this.dvFields)
+	
+			dojo.forEach(this.fieldset, function(definition) {
+				var label = dojo.create("div", {innerHTML: definition.name}) 
+				dojo.addClass(label, "label")
+				if (definition.cssClass) dojo.addClass(label, definition.cssClass + "-label")
+				dojo.place(label, dv)
+			})
+		}
+	},
+	
+	/**
+	 * 
+	 * @param {integer} rowNumber	serial number of the row
+	 * @param {true} bottomRow		true if this is the placeholder bottom row
+	 */
+	_addSet: function(rowNumber, bottomRow) {
+		var self = this
+		
+		if (this.type == this._SUBFORM) 
+			if (!bottomRow)
+			 if (rowNumber>=(this.cardinality.max-1)) this._hideAddButton(true)
+		
+		var dv
+		if (bottomRow) dv = this.dvActions;
+		else {
+			this._rows++
+			var dv = dojo.create("div")
+			dojo.addClass(dv, "set")
+			dojo.addClass(dv, this.style)
+			dojo.place(dv, this.dvFields)
+			
+			self._animationInProgress = true
+
+			dojo.style(dv, "opacity", "0")
+			dojo.animateProperty({
+				node: dv, 
+				duration: 500,
+				properties: {
+					opacity: 1			
+				},
+				onEnd: function() {
+					self._animationInProgress = false
+				}
+			}).play()
+		}
 		
 		dojo.forEach(this.fieldset, function(definition) {
 			var field 			
-			
+
 			attr = {
 				name: definition.name
 			}
-
-			if (definition.type) {
-				switch (definition.type) {
+			
+			if (definition.type == self._SUBFORM) {
+				definition.formGenerator = self.formGenerator
+				field = new apstrata.widgets.forms.FieldSet(definition)
+			} else if (definition.widget) {
+				switch (definition.widget) {
 					case "dijit.form.ComboBox":
 					
 						var choices = []
@@ -48,48 +182,82 @@ dojo.declare("apstrata.widgets.forms.FieldSet",
 						
 						field = new dijit.form.ComboBox(attr)
 						break;
-						
-					case "subform":
-						field = new apstrata.widgets.forms.FieldSet(definition)
-					
-						break;
 									
 					default:
 						field = new dijit.form.TextBox(attr)
 						break;
 				}
+				
+				//field.set("name", definition.name)
 			} else {
 				field = new dijit.form.TextBox(attr)
+				//field.set("name", definition.name)
 			}
 			
-			dojo.place(dojo.create("div", {innerHTML: definition.name}), dv)
+			// If this is not a tabular format add a label before each field
+			if (self.style == self._FORM) {
+				var label = dojo.create("div", {innerHTML: definition.name}) 
+				dojo.addClass(label, "label")
+				dojo.place(label, dv)
+			}
+			
+			if (definition.cssClass) dojo.addClass(field.domNode, definition.cssClass)
+			if (bottomRow) field.set("disabled", "disabled")
+			
 			dojo.place(field.domNode, dv)
 		})
 		
-		if (this._type == "subform") {
-			var button = new dijit.form.Button({
-				label: "-",
-				onClick: function() {
-					dv.parentNode.removeChild(dv)
+		if (!bottomRow && (this.type == this._SUBFORM)) {
+			if (rowNumber>=this.cardinality.min) {
+				var button = new dijit.form.Button({
+					label: "-",
+					onClick: function() {
+						self._remove(dv, self)
+					}
+				})
+				dojo.place(button.domNode, dv)
+			}
+		}
+
+		if (bottomRow) {
+			if (!this.cardinality.max || (this.cardinality.max > this.cardinality.min)) {
+				if (this.type == "subform") {
+					self._addButton = new dijit.form.Button({
+						label: "+",
+						onClick: function(){
+							self._addSet(self._rows)
+						}
+					})
+					dojo.place(self._addButton.domNode, dv)
 				}
-			})
-			dojo.place(button.domNode, dv)
+			}
 		}
 	},
-	
-	postCreate: function(options) {
-		var self = this
-		this.addSet()
 
-		if (this._type == "subform") {
-			var button = new dijit.form.Button({
-				label: "+",
-				onClick: function() {
-					self.addSet()
-				}
-			})
-			dojo.place(button.domNode, this.dvActions)
-		}
+	/**
+	 * Delete a row
+	 * 
+	 * @param {DOMNode} dv		containing div
+	 * @param {Object} self		FieldSet widget		
+	 */
+	_remove: function(dv, self) {
+		self._animationInProgress = true
+		
+		self.formGenerator.disableButtons(true)
+		
+		dojo.animateProperty({
+			node: dv, 
+			duration: 250,
+			properties: {
+				opacity: 0
+			},
+			onEnd: function() {
+				self.formGenerator.disableButtons(false)
+				dv.parentNode.removeChild(dv)
+				self._rows--
+				if (self._rows<self.cardinality.max) self._hideAddButton(false)
+			}
+		}).play()
 	}
 	
 })
--- apstrataDojoSDK/apstrata/widgets/forms/FormGenerator.js
+++ apstrataDojoSDK/apstrata/widgets/forms/FormGenerator.js
@@ -2,14 +2,12 @@ dojo.provide("apstrata.widgets.forms.FormGenerator")
 
 dojo.require("dojox.dtl._Templated")
 
-dojo.require("dojo.store.Memory")
-dojo.require("dojo.data.ObjectStore")
-
 dojo.require("dijit.form.Form")
 
 dojo.require("apstrata.widgets.forms.FieldSet")
 
 /**
+ * Generates a dijit.form.Form automatically based on a definition object
  * 
  * @param {Object} attrs
  */
@@ -22,7 +20,16 @@ dojo.declare("apstrata.widgets.forms.FormGenerator",
 	definition: null,
 	definitionPath: "",
 	label: "",
+	value: null,
 	
+	/**
+	 * Instantiates a FormGenerator that will generate a dynamic form widget based on options.definition 
+	 * 
+	 * @param {Object} options.definition 		form Definition object
+	 * @param {string} options.definitionPath	URL to the form definition  
+	 * 
+	 * @construct
+	 */
 	constructor: function(options) {
 		if (options.definition) {
 			this.definition = options.definition
@@ -32,29 +39,120 @@ dojo.declare("apstrata.widgets.forms.FormGenerator",
 	},
 	
 	postCreate: function() {
+		// Build the form only after loading the definition
 		this._getdefinition().then(dojo.hitch(this,"_buildForm"), dojo.hitch(this,"_error"))
 	},
+	
+	//
+	// Publich methods
+	//
+	
+	/**
+	 * This method disables all buttons on a form, 
+	 * 	it is useful when doing an async operation or animation and need to stop the user from doing any
+	 * 	action until it's done
+	 * 
+	 * @param {boolean} disabled
+	 */
+	disableButtons: function(disabled) {
+		dojo.query(".dijitButton", this.domNode).forEach(function(node, index, arr){
+		    dijit.byId(dojo.attr(node, "widgetid")).set("disabled", disabled?"disabled":"")
+		})
+	},
+
+	//
+	// Events
+	//
+	
+	/**
+	 * Is called when an action button is pressed
+	 * 
+	 * @param {string} action 	Action button label
+	 * @param {Object} values 	form values
+	 */
+	onAction: function(action, values) {},
 	
 	//
 	// Private methods
 	//
 	
+	/**
+	 * Load the definition from a url at definitionPath is provided and definition is not
+	 */
 	_getdefinition: function() {
 		var deferred = new dojo.Deferred()
 		if (this.definition) deferred.resolve()
 		return deferred
 	},
 
+	/**
+	 * Generate the form based on definition
+	 */
 	_buildForm: function() {
+		var self = this
+		
+		// add custom CSS class
+		//  TODO: this is not working go figure!
+		if (this.definition.cssClass) dojo.addClass(this.domNode, this.definition.cssClass)
+		
+		// If the user has provided a label show it
 		if (this.definition.label) this.label = this.definition.label
 		this.render()
+
+		// This is important to set the appropriate number of rows in a multivalue field
+		//	based on this.value
+		this.cardinalityValues = {}
+		for (key in this.value) {
+			// The starting cardinality depends on how many values were set for each attribute 
+			if (dojo.isArray(value[key])) this.cardinalityValues[key] = value[key].length; 
+				// if it's not an array then the cardinaltity value is 1
+				else this.cardinalityValues[key] = 1
+		}		
 		
+		// Start building the form
 		this._rootFieldSet = new apstrata.widgets.forms.FieldSet({
-			type: "main", style: "single",
-			fieldset: self.definition.fieldset
+			type: "form", style: "form",
+			fieldset: self.definition.fieldset,
+			value: self.value,
+			formGenerator: self
 		}, this.dvFieldset)
+		
+		this._addActions()
+		
+		// Dijit.form.Form will do the heavylifting to set the values on all fields
+		if (this.value) this.frmMain.set("value", this.value)
 	},
 	
+	/**
+	 * Add the actions at the bottom of the form based on definition.actions
+	 */
+	_addActions: function() {
+		var self = this
+		
+		dojo.forEach(self.definition.actions, function(action) {
+			var button = new dijit.form.Button({
+				label: action,
+				onClick: function() {
+					
+					var values = self.frmMain.get("value")
+
+					// get rid of the undefined values,
+					//  TODO: don't know why we're getting undefined values by the name of the FieldSets					
+					for (k in values) {
+						if (values[k] == undefined) delete values[k]
+					}
+					console.dir(values)
+					self.onAction(action, values)
+				}
+			})
+			
+			dojo.place(button.domNode, self.dvActions)
+		})
+	},
+	
+	/**
+	 * Display an error in case we can't load the definition
+	 */
 	_error: function() {
 	}
 
