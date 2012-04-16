29352264493a1fd247d3bca035aacb36e64239bb
rabih.nassar@elementn.com <rabih.nassar@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2012-04-16 11:50:02 +0000

    removing non maintained demo applications: Explorer, genericEmbeddable, mForms, presnetation, survey

--- apstrataDojoSDK/demos/genericEmbeddable/apstrata/genericEmbeddable/GenericEmbeddable.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author rabih
- */
-dojo.declare("apstrata.genericEmbeddable.GenericEmbeddable")
-
-dojo.define("apstrata.genericEmbeddable.GenericEmbeddable", [dijit._Widget, dijit._Templated], {
-	templateString: "<div><b>I'm embedded</b></div>"
-})
