8e60271645377e3b7f24057576c3629e4ac4eeb6
rabih.nassar@elementn.com <rabih.nassar@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2012-04-30 20:58:00 +0000

    added test-CRUD test code

--- apstrataDojoSDK/apstrata/sdk/ApConfig.js
+++ apstrataDojoSDK/apstrata/sdk/ApConfig.js
@@ -27,9 +27,8 @@ dojo.provide('apstrata.sdk.ApConfig');
 dojo.setObject("apstrata.apConfig", {
 	get: function() {return this},
 	credentials: {
-		key:'RA422AED62',
-//		key:'E83D22E93F',
-		secret:'QC8EDBE4ABCD5F97074B8B59B21C5C65'
+		key:'',
+		secret:''
 	},
 	serviceURL: 'http://sandbox.apstrata.com/apsdb/rest',
 	defaultStore: '',
