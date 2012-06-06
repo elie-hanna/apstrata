c4bebc284428c4cc6778a412b2ef2b30ecbaa1e1
rabih.nassar@elementn.com <rabih.nassar@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2012-06-06 16:19:17 +0000

    initial commit

---
+++ ApstrataCMS/manage/config.php
@@ -0,0 +1,31 @@
+<?php
+
+	$config = array (
+		"apstrataServiceURL" => "https://apsdb.apstrata.com/sandbox-apsdb/rest",
+		"apstrataKey" => "E83D22E93F",
+		"contentStore" => "apstrata",
+		"crmStore" => "",
+		"developmentMode" => true,
+		"useStub" => true,
+		"apstrataConnectionTimeout" => 30000,
+		"urlPrefix" => "page.php?pageId=",
+		"template" => "min",
+		"cachingHeaders" => "false",
+		"cachingAge" => 3000
+	);
+	
+	// Dublin Core meta-data site-wide init
+	$config["DC"] = array (
+		"Title" => "apstrata",
+		"Type" => "cloud, back end as a service, BaaS, mobile cloud, HTML5 cloud",
+		"Description" => "HTML5 and mobile cloud back-end",
+		"Subject.keyword" => "elementn, element&circ;n, apstrata, html5, saas, paas, baas, cloud computing, rich client, RIA",
+		"Version" => "1",
+		"Publisher" => "apstrata",
+		"Creator" => "apstrata",
+		"Creator.Address" => "apstrata",
+		"Identifier" => "http://www.apstrata.com/",
+		"Rights" => "Copyright, Copyright Statement (http://www.apstrata.com/terms)."
+	);
+?>
+
