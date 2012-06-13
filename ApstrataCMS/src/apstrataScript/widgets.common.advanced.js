c5fd305a37e16209f9b779b09e1d18ae12544a4f
karim.saikali@elementn.com <karim.saikali@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2012-06-13 16:52:53 +0000

    'JIRA ID [APSTRATA-2268]'
    removed k/s

--- ApstrataCMS/src/apstrataScript/widgets.common.advanced.js
+++ ApstrataCMS/src/apstrataScript/widgets.common.advanced.js
@@ -10,9 +10,9 @@
  * The below variables will be populated at deployment time with the correct values
  */
 
-var accountCreationEnvironment = "https://test-apps.apstrata.com";
-var accountCreationKey = "WC7A01F4C8";
-var accountCreationSecret = "TC8F2E246874CB3C86856C33BCB2047F";
+var accountCreationEnvironment = "";
+var accountCreationKey = "";
+var accountCreationSecret = "";
  
 var advancedConfiguration = {	
 	accountCreationEnvironment: accountCreationEnvironment,	
