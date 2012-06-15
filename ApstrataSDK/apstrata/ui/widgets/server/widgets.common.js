4a69aa642e01014161eec8292c7a39660e80e549
karim.saikali@elementn.com <karim.saikali@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2012-06-15 07:22:58 +0000

    'JIRA ID [APSTRATA-2259]'
    Added comments

--- ApstrataSDK/apstrata/ui/widgets/server/widgets.common.js
+++ ApstrataSDK/apstrata/ui/widgets/server/widgets.common.js
@@ -10,14 +10,34 @@
  * The below variables will be populated at deployment time with the correct values
  */
 
+// The name of the project (or site) the user is subscribing to
 var projectName = "MyProject";
+
+// The name of the default group to which subscribing users will belong once registered
 var defaultUsersGroup = "users";
+
+// The store where temporary registration documents will be created on the client application (user registration and/or account provisioning))
 var defaultUnconfirmedRegistrationStore = "DefaultStore";	
+
+// The auth key of the provisioning account on the client application
 var apstrataHomeEnvironment = "";
+
+// The auth key of the provisioning account on the client application
 var apstrataHomeKey = "";
+
+// The secret of the provisioning account on the client application
+var apstrataHomeSecret = "";
+
+// The type of registration that is required ("user" -> for user registration, or "account" -> for account creation)
 var registrationType = "user";
+
+// Specify if an email should be sent to the user upon successful registration (true/false)
 var sendEmailOnceRegistrationConfirmed = true;
+
+// The URL that is sent in the verification e-mail for the subscriber to click on (you normally should not change this)
 var verifyUrl = apstrataHomeEnvironment + "/apsdb/rest/" + apstrataHomeKey + "/RunScript?apsws.authMode=simple&apsdb.scriptName=widgets.Registration.verifyAccount&login=$login&d=$confirmation";
+
+// The URL where to redirect the user after he submits his registration form
 var registrationRedirectUrl = ""
  
 var configuration = {
