8bd5e86a45a88802d4ddf820aeb53ed5ee867670
walid.bakkar@elementn.com <walid.bakkar@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2012-06-22 14:31:55 +0000

    JIRA ID[APSTRATA-2228] workbench packaging: assume packaged if apstrata is already defined.

--- ApstrataSDK/apstrata/sdk/apstrata.js
+++ ApstrataSDK/apstrata/sdk/apstrata.js
@@ -17,127 +17,131 @@
  *  along with Apstrata Database Javascript Client.  If not, see <http://www.gnu.org/licenses/>.
  * *****************************************************************************
  */
+var packaged = true;
 
-if (typeof apstrata == "undefined" || !apstrata.configured) {
+if (typeof apstrata == "undefined") {
+	packaged = false;
+}
+
+if (typeof apstrata == "undefined") apstrata = {}
 	
-	if (typeof apstrata == "undefined") apstrata = {}
-		
-	apstrata.configured = true;
+apstrata.configured = true;
+
+apstrata.baseUrl = ""
 	
-	apstrata.baseUrl = ""
-		
-	apstrata.version = {
-		major: 1,
-		minor: 0,
-		patch: 0,
-		flag: "",
-		revision: "$revision$",
-		version: "$version$-$tag$",
-		toString: function(){
-			with (apstrata.version) {
-				return major + "." + minor + "." + patch + flag + " (" + revision + ")"; // String
-			}
+apstrata.version = {
+	major: 1,
+	minor: 0,
+	patch: 0,
+	flag: "",
+	revision: "$revision$",
+	version: "$version$-$tag$",
+	toString: function(){
+		with (apstrata.version) {
+			return major + "." + minor + "." + patch + flag + " (" + revision + ")"; // String
 		}
 	}
-	
-	
-	// Find out where apstrata.js is, this is the root folder for the apstrata core package
-	//  Extract apConfig also
-	if ((this["document"]) && (this["document"]["getElementsByTagName"])) {
-		var scripts = document.getElementsByTagName("script");
-		var rePkg = /apstrata(-lib)?\.js([\?\.]|$)/i;
-		for (var i = 0; i < scripts.length; i++) {
-			// var src = scripts[i].getAttribute("src");
-			var src = scripts[i].src; // TODO: is this portable?
-			if (!src) {
-				continue;
+}
+
+
+// Find out where apstrata.js is, this is the root folder for the apstrata core package
+//  Extract apConfig also
+if ((this["document"]) && (this["document"]["getElementsByTagName"])) {
+	var scripts = document.getElementsByTagName("script");
+	var rePkg = /apstrata(-lib)?\.js([\?\.]|$)/i;
+	for (var i = 0; i < scripts.length; i++) {
+		// var src = scripts[i].getAttribute("src");
+		var src = scripts[i].src; // TODO: is this portable?
+		if (!src) {
+			continue;
+		}
+		
+		var m = src.match(rePkg);
+		if (m) {
+			apstrata.baseUrl = src.substring(0, m.index-1);
+			
+			// Making sure to get the full path to apstrata.js
+		   	var el= document.createElement('div');
+			var escapedUrl = apstrata.baseUrl.split('&').join('&amp;').split('<').join('&lt;').split('"').join('&quot;');
+	        el.innerHTML= '<a href="'+escapedUrl+'">x</a>';
+	        apstrata.baseUrl = el.childNodes[0].href;
+			
+			if (scripts[i].getAttribute("apConfig")) {
+				var s = "{" + scripts[i].getAttribute("apConfig") + "}"
+				var o = dojo.fromJson(s)
+				if (typeof o != undefined) 
+					apstrata["apConfig"] = o
+			}
+			break;
+		}
+	}
+}
+
+// Find out where dojo.js and calculate the relative path from dojo to the current location
+if ((this["document"]) && (this["document"]["getElementsByTagName"])) {
+	var scripts = document.getElementsByTagName("script");
+	var rePkg = /dojo\.js([\?\.]|$)/i;
+	for (var i = 0; i < scripts.length; i++) {
+		// var src = scripts[i].getAttribute("src");
+		var src = scripts[i].src; // TODO: is this portable?
+		if (!src) {
+			continue;
+		}
+		var m = src.match(rePkg);
+		if (m) {
+			
+			// Making sure to get the full path to dojo.js
+		   	var el= document.createElement('div');
+			var escapedUrl = src.split('&').join('&amp;').split('<').join('&lt;').split('"').join('&quot;');
+	        el.innerHTML= '<a href="'+escapedUrl+'">x</a>';
+	        src = el.childNodes[0].href;
+		
+			// split the path into an array  
+			var p1 = src.split('/')
+			var p2 = (this.location+"").split('/')
+
+			// find out at what index the 2 arrays are not identical
+			var j=0
+			for (; j<p1.length; j++) {
+				if (p1[j] != p2[j]) break;
+			}
+
+			// reconstruct the relative path from dojo of the current location 
+			//  the page has loaded from
+			var s = ""
+			// find out the common root
+			for (var k=0; k<(p1.length-j-1); k++) {
+				s += "../"
 			}
 			
-			var m = src.match(rePkg);
-			if (m) {
-				apstrata.baseUrl = src.substring(0, m.index-1);
-				
-				// Making sure to get the full path to apstrata.js
-			   	var el= document.createElement('div');
-				var escapedUrl = apstrata.baseUrl.split('&').join('&amp;').split('<').join('&lt;').split('"').join('&quot;');
-		        el.innerHTML= '<a href="'+escapedUrl+'">x</a>';
-		        apstrata.baseUrl = el.childNodes[0].href;
-				
-				if (scripts[i].getAttribute("apConfig")) {
-					var s = "{" + scripts[i].getAttribute("apConfig") + "}"
-					var o = dojo.fromJson(s)
-					if (typeof o != undefined) 
-						apstrata["apConfig"] = o
-				}
-				break;
+			// add the path to the location
+			for (k=j; k<(p2.length-1); k++) {
+				s += p2[k] + "/"
 			}
-		}
-	}
-
-	// Find out where dojo.js and calculate the relative path from dojo to the current location
-	if ((this["document"]) && (this["document"]["getElementsByTagName"])) {
-		var scripts = document.getElementsByTagName("script");
-		var rePkg = /dojo\.js([\?\.]|$)/i;
-		for (var i = 0; i < scripts.length; i++) {
-			// var src = scripts[i].getAttribute("src");
-			var src = scripts[i].src; // TODO: is this portable?
-			if (!src) {
-				continue;
-			}
-			var m = src.match(rePkg);
-			if (m) {
-				
-				// Making sure to get the full path to dojo.js
-			   	var el= document.createElement('div');
-				var escapedUrl = src.split('&').join('&amp;').split('<').join('&lt;').split('"').join('&quot;');
-		        el.innerHTML= '<a href="'+escapedUrl+'">x</a>';
-		        src = el.childNodes[0].href;
+							
+			apstrata.pathFromDojo = s
 			
-				// split the path into an array  
-				var p1 = src.split('/')
-				var p2 = (this.location+"").split('/')
-
-				// find out at what index the 2 arrays are not identical
-				var j=0
-				for (; j<p1.length; j++) {
-					if (p1[j] != p2[j]) break;
-				}
-
-				// reconstruct the relative path from dojo of the current location 
-				//  the page has loaded from
-				var s = ""
-				// find out the common root
-				for (var k=0; k<(p1.length-j-1); k++) {
-					s += "../"
-				}
-				
-				// add the path to the location
-				for (k=j; k<(p2.length-1); k++) {
-					s += p2[k] + "/"
-				}
-								
-				apstrata.pathFromDojo = s
-				
-				break;
-			}
+			break;
 		}
 	}
+}
 
+/*
+ * IE does not support console.groupCollapsed() and console.groupEnd() and console.dir() functions
+ * Hence, override these functions with empty ones in case of IE 
+ */
+if (dojo.isIE) {
+	console.groupCollapsed = function(groupName) {}
+	console.groupEnd = function() {}
+	console.dir = function() {}
+} 
+
+if(!packaged){
 	dojo.registerModulePath("apstrata", apstrata.baseUrl + "/..")
 	
 	dojo.registerModuleRelative = function(module, string) {
 		dojo.registerModulePath (module, apstrata.pathFromDojo + string)
 	}
-	
-	dojo.require("apstrata.sdk.Registry")
-
-	/*
-	 * IE does not support console.groupCollapsed() and console.groupEnd() and console.dir() functions
-	 * Hence, override these functions with empty ones in case of IE 
-	 */
-	if (dojo.isIE) {
-		console.groupCollapsed = function(groupName) {}
-		console.groupEnd = function() {}
-		console.dir = function() {}
-	} 
 }
+
+dojo.require("apstrata.sdk.Registry")
