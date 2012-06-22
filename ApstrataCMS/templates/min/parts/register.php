a55cb73925a507795efa8e51748c34e60dc5a479
karim.saikali@elementn.com <karim.saikali@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2012-06-22 15:52:48 +0000

    'JIRA ID [APSTRATA-2259]'
    Extracting login url from config.php

--- ApstrataCMS/templates/min/parts/register.php
+++ ApstrataCMS/templates/min/parts/register.php
@@ -6,7 +6,7 @@
 	
 		<script type="text/javascript" src="lib/ApstrataSDK/apstrata/ui/embed/apstrataEmbed.js"></script>
 
-		<div class="registrationWidget embed spinner" data-apstrata-embedType="apstrata.ui.widgets.RegistrationWidget" loginurl="/ApstrataCMS/page.php?pageId=dashboard"></div>
+		<div class="registrationWidget embed spinner" data-apstrata-embedType="apstrata.ui.widgets.RegistrationWidget" loginurl='<?php print $GLOBALS["config"]["loginUrl"]; ?>'></div>
 	</div>
 </div>
 <!-- embed code: end -->
