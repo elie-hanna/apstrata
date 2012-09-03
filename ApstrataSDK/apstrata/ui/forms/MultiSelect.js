68883e0ce5e8c8a2fcbf7a73ca8642f89d7ad469
marvette.feghali@elementn.com <marvette.feghali@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2012-09-03 10:11:11 +0000

    Widgets that extends dijit.form.MultiSelect. This extension will add  the possibility to programatically define a MultiSelect by passing to it an options array of objects in the format [{id:"option1", name:"optionLabel"},{id:"option2", name:"option2Label"}]

---
+++ ApstrataSDK/apstrata/ui/forms/MultiSelect.js
@@ -0,0 +1,32 @@
+dojo.provide("apstrata.ui.forms.MultiSelect")
+
+dojo.require("dojox.dtl._Templated");
+dojo.require("dijit.form.MultiSelect");
+
+dojo.declare("apstrata.ui.forms.MultiSelect", 
+[dijit.form.MultiSelect], 
+{
+	/*
+	 * Options that will populate the select list
+	 * example:
+	 * 	[{id:"value1", name:"label1"},{id:"value2", name: "label2"}] 
+	 */
+	options: [],
+
+	/*
+	 * Extends the dijit.form.MultiSelect, to be used programatically by passing to it a set of options.
+	 * Build the passed options array as HTML options and appends them to the MultiSelect empty domNode
+	 */
+	postCreate: function() {		
+		if (this.options) {
+			var self = this;
+			dojo.forEach(this.options, function(option) {
+		            var c = document.createElement('option');
+		            c.innerHTML = option.name;
+		            c.value = option.id;
+		            self.domNode.appendChild(c);
+			});
+		}
+		this.inherited(arguments)
+	}
+});
