f7deeedc75346d815cbeb9f74ff67460127cbdbf
karim.saikali@elementn.com <karim.saikali@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2012-10-17 14:44:22 +0000

    JIRA ID [APSTRATA-2362] Subscription confirmed message updated

--- ApstrataSDK/apstrata/ui/widgets/server/widgets.common.js
+++ ApstrataSDK/apstrata/ui/widgets/server/widgets.common.js
@@ -11,22 +11,22 @@
  */
 
 // The name of the project (or site) the user is subscribing to
-var projectName = "MyProject";
+var projectName = "Apstrata Web Site";
 
 // The name of the default group to which subscribing users will belong once registered
 var defaultUsersGroup = "users";
 
 // The store where temporary registration documents will be created on the client application (user registration and/or account provisioning))
-var defaultUnconfirmedRegistrationStore = "DefaultStore";	
+var defaultUnconfirmedRegistrationStore = "pendingRegistrations";	
 
 // The auth key of the provisioning account on the client application
-var apstrataHomeEnvironment = "";
+var apstrataHomeEnvironment = "https://varick.apstrata.com";
 
 // The auth key of the provisioning account on the client application
-var apstrataHomeKey = "";
+var apstrataHomeKey = "E83D22E93F";
 
 // The secret of the provisioning account on the client application
-var apstrataHomeSecret = "";
+var apstrataHomeSecret = "XEA48AE769BF2B86910FBC74116CDF4E";
 
 // The type of registration that is required ("user" -> for user registration, or "account" -> for account creation)
 var registrationType = "account";
@@ -35,10 +35,10 @@ var registrationType = "account";
 var sendEmailOnceRegistrationConfirmed = true;
 
 // The URL that is sent in the verification e-mail for the subscriber to click on (you normally should not change this)
-var verifyUrl = apstrataHomeEnvironment + "/" + apstrataHomeKey + "/RunScript?apsws.authMode=simple&apsdb.scriptName=widgets.Registration.verifyAccount&login=$login&d=$confirmation";
+var verifyUrl = apstrataHomeEnvironment + "/apsdb/rest/" + apstrataHomeKey + "/RunScript?apsws.authMode=simple&apsdb.scriptName=widgets.Registration.verifyAccount&login=$login&d=$confirmation";
 
 // The URL where to redirect the user after he submits his registration form
-var registrationRedirectUrl = ""
+var registrationRedirectUrl = "http://www.apstrata.com/page.php?pageId=processingReg"
  
 var configuration = {
 	projectName: projectName,
@@ -59,7 +59,7 @@ var configuration = {
 	templatesConfirmed: {
 		adminEmail: "dude@dude.com",
 		subject: "$projectName Registration confirmed",
-		body: "<div style='font-family:Calibri, font-size:11'>Dear $user,<br/><br/>Welcome to Apstrata.  You now have access to your Apstrata account.<br/>Please use the the email and password you provided to login to the Apstrata site.<br/><br/>The home page of the apstrata.com website is a live link into the documentation.  Simply click on an icon to find out more about that feature.<br/>In addition, here are some links you may find useful:<ul><li>Documentation can be found<a href='http://wiki.apstrata.com/'> here.</a></li><li>Language-specific API clients can be downloaded <a href='http://code.google.com/p/apstrata/source/browse/#svn'> here.</a></li><li>The Developer Workbench can be found <a href='http://workbench.apstrata.com/'>here.</a></li><br/></ul>IF YOU NEED ASSISTANCE: email us at support@apstrata.com and we will respond within one business day.<br/><br/>Thank you for using Apstrata.  We got your back.<br/>The Apstrata team"
+		body: "<div style='color: #003080;font: 14px Myriad,Helvetica,Tahoma,Arial,clean,sans-serif;'><div style='background-color: #003080;margin-bottom: 10px;'><img style='padding-left: 10px;' src='https://sandbox.apstrata.com/apsdb/rest/E83D22E93F/GetFile?apsws.time=1349340156020&apsws.authSig=dfd97bc84026e3004ada2613d40d8b77&apsws.responseType=json&apsws.authMode=simple&apsdb.fileName=apstratalogo.png&apsdb.store=apstrata&apsdb.documentKey=ApstrataLogo&apsdb.fieldName=apsdb_attachments'></div>Dear $user,<br><br><span style='font-size:1.5em;line-height:200%'>Welcome to Apstrata.</span><br/>You now have access to your Apstrata account. Please use the email and password you provided to login to the Apstrata site.<br><br>The home page of the <a href='www.apstrata.com'>Apstrata website</a> is a live link into the documentation. Simply click on an icon to find out more about that feature.<br><br>In addition, here are some links you may find useful:<br><br><ul><li>Documentation can be found <a href='http://wiki.apstrata.com'>here</a><br><br></li><li>Language-specific API clients can be downloaded <a href='http://code.google.com/p/apstrata/downloads/list'>here</a><br><br></li><li>The Developer Workbench can be found <a href='http://workbench.apstrata.com'>here</a><br><br></li></ul><span style='font-size:1.5em'>If you need assistance:</span><br><br> email us at <b>support@apstrata.com </b>and we will respond within one business day.<br><br>Thank you for using Apstrata.<br><br> We got your back.<br>The Apstrata team</div>"
 	}
 }
 
