78e4063f60c8c4d5bdd795a9b5ff0a61dbd62f6a
rabih.nassar@elementn.com <rabih.nassar@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2012-12-02 22:16:38 +0000

    test implementation for catalog pages

--- ApstrataCMS/templates/min/parts/catalog.php
+++ ApstrataCMS/templates/min/parts/catalog.php
@@ -10,6 +10,16 @@
 	if (isset($page["firstQuery"])) $firstQuery= json_decode($page["firstQuery"]);
 	if (isset($page["secondQuery"])) $secondQuery= json_decode($page["secondQuery"]);
 
+	$page1 = 1;
+	if (isset($_GET['catalog-page1'])){
+	   $page1 = $_GET['catalog-page2'];
+	}
+
+	$page2 = 1;
+	if (isset($_GET['catalog-q2-page'])){
+	   $page2 = $_GET['catalog-q2-page'];
+	}
+
 ?>
 <div class='page catalogPage'>
 	<h2><?php print $title; ?></h2>
@@ -17,6 +27,7 @@
 	<div class="pageDescription"><?php print $description; ?></div>
 
 
+	<div>first query: page <?php print $page1; ?> of <?php print ceil($firstQuery->count/11); ?></div>
 	<div class='slides'>
 		<?php
 				foreach ($firstQuery->documents as $item) {
@@ -36,6 +47,8 @@
 	
 	<div style="clear: both;"></div>
 	
+	<br>
+	<div>second query: page <?php print $page2; ?> of <?php print ceil($firstQuery->count/11); ?></div>
 	<?php
 			foreach ($secondQuery->documents as $item) {
 	?>
