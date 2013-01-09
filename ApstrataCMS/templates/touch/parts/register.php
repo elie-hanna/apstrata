27a8cb576ec2abcf50ddb691f40f73b00e103488
karim.saikali@elementn.com <karim.saikali@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2013-01-09 10:13:59 +0000

    JIRA ID [APSTRATA-2399] Fixed bug from previous site

--- ApstrataCMS/templates/touch/parts/register.php
+++ ApstrataCMS/templates/touch/parts/register.php
@@ -7,7 +7,7 @@
 		<script type="text/javascript" src="lib/ApstrataSDK/apstrata/ui/embed/apstrataEmbed.js"></script>
 
 		<div class="registrationWidget embed spinner" data-apstrata-embedType="apstrata.ui.widgets.RegistrationWidget" 
-			loginurl='<?php echo $GLOBALS["config"]["baseUrl"] . "/". $GLOBALS["config"]["urlPrefix"] . $GLOBALS["config"]["loginUrl"]; ?>'</div>
+			loginurl='<?php echo $GLOBALS["config"]["baseUrl"] . "/". $GLOBALS["config"]["urlPrefix"] . $GLOBALS["config"]["loginUrl"]; ?>'></div>
 	</div>
 </div>
 <!-- embed code: end -->
