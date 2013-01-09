ef60be2759a4f4db9b0c6b73cc3870280e57ef54
karim.saikali@elementn.com <karim.saikali@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2013-01-09 10:15:04 +0000

    JIRA ID [APSTRATA-2399] Added social media (incomplete)

--- ApstrataCMS/templates/touch/parts/top.php
+++ ApstrataCMS/templates/touch/parts/top.php
@@ -2,10 +2,27 @@
 <div class="header-wrap">
 	<header>
 		<div id="logo">
-			<a href="<?php print $GLOBALS["config"]["baseUrl"]; ?>" id='logo'>
+			<a href="<?php print $GLOBALS["config"]["baseUrl"]."/page.php?pageId=home"; ?>" id='logo'>
 				<img src="themes/touch/images/logo.png" alt="Touch Cloud" title="Touch Cloud" />
 			</a>
 		</div>
+		<!-- begin credentials-->
+        <div id="credentials">
+                <ul>
+                    <li class="login"><a href=<?php echo $config["baseUrl"]."/page.php?pageId=dashboard"; ?>>Login</a></li>
+                    <li><a href=<?php echo $config["baseUrl"]."/page.php?pageId=register"; ?>>Signup</a></li>
+                </ul>
+        </div>
+		<!-- begin social media -->
+        <div class="social-media">
+            <ul>
+                <li class="facebook"><a href="#">facebook</a></li>
+                <li class="twitter"><a href="#">twitter</a></li>
+                <li class="linkedin"><a href="#">linkedin</a></li>
+                <li class="youtube"><a href="#">youtube</a></li>
+            </ul>
+        </div>
+        <!-- end social media -->
 	</header>
 </div>      
 <div id="nav-wrapper">
