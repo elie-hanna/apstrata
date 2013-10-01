da70f75cb9cccfee54a486a1da1df0d832bd684f
eliana.morkos@elementn.com <eliana.morkos@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2013-10-01 11:31:38 +0000

    JIRA ID [AMC-523]
    - Set up a login page outside pre-existing dashboard
    - add header login/logout links widget
    - add horizon structure after login
    - integrate a min footer for logged in users

---
+++ ApstrataCMS/src/home/Logging.js
@@ -0,0 +1,50 @@
+dojo.provide("apstrata.home.Logging");
+
+dojo.require("dojox.dtl._Templated");
+dojo.require("dijit._Widget");
+
+dojo.require("apstrata.sdk.TokenConnection")
+
+dojo.declare("apstrata.home.Logging",
+[dijit._Widget, dojox.dtl._Templated], 
+{
+	signupUrl: '',
+	signupLabel: 'Signup',
+	
+	loginLabel: 'Login',
+	loginUrl:'',
+	
+	logoutLabel: 'Log out',
+	logoutUrl: '',
+	
+	isLoggedIn: false,
+	credentials: {},
+	
+	widgetsInTemplate: false,
+	templatePath: dojo.moduleUrl("apstrata.home", "templates/Logging.html"),
+	
+	
+	constructor: function(attrs){
+		dojo.mixin(this, attrs);
+		
+		var theConnection = new apstrata.sdk.TokenConnection();
+		this.isLoggedIn = theConnection.isLoggedIn();
+	},
+	
+	logout: function() {
+		var self = this;
+		var connection = new apstrata.sdk.TokenConnection({credentials: this.credentials, isUseParameterToken: true});	
+		if (connection) {
+			connection.logout({
+				success: function() {
+					window.location = self.logoutUrl;	
+				},
+				failure: function() {
+					
+				}
+			})		
+		}
+	}	
+	
+	
+})
