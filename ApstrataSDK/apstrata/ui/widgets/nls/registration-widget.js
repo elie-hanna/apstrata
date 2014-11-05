98945c7c9fa08b2985fa3b8eedb8fad1b0916fc4
elie.youssef@elementn.com <elie.youssef@elementn.com@4bf3bd18-1a23-11de-a78b-a7aca27b2395>
2014-11-05 08:18:01 +0000

    Captcha and Password Strength Implementation

--- ApstrataSDK/apstrata/ui/widgets/nls/registration-widget.js
+++ ApstrataSDK/apstrata/ui/widgets/nls/registration-widget.js
@@ -2,7 +2,7 @@ define({ root:
 //begin v1.x content
 ({
 	ENTER_VALID_EMAIL: "Enter valid email address",
-	ENTER_VALID_PASSWORD: "Password must be 8 characters containing at least one number, one lowercase and one uppercase letter ",
+	ENTER_VALID_PASSWORD: "Password must be at least 8 characters containing at least one number, one lowercase and one uppercase letter Because I was able to add a password with more than 8 characters",
 	EMAIL_ALREADY_REGISTERED: "Email already registered",
 	WRONG_CAPTCHA: "Wrong Captcha",
 	LOGIN: "login?"
